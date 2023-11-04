dfcedecb92adbb92f3050dd775d3164e9cb90243
aishwaryar20115 <122954982+aishwaryar20115@users.noreply.github.com>
2023-11-04 19:17:49 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get('/message', (req, res) => {
+    res.json({ message: "Do you want to restart the game.." });
+});
+
+app.listen(8000, () => {
+    console.log(`Server is running on port 8000.`);
+  });
